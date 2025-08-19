a9f6748f34807cadfca25c11cdfd0ae2a0dc6b2d
Dario Gasciarino <dario.gasciarino@outlook.it>
2025-08-19 17:13:06 +0200

    refactor(core): export decoders barrel file

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,5 +1,6 @@
 export 'assets/assets.dart';
 export 'caching/caching.dart';
+export 'decoders/decoders.dart';
 export 'di/dependency_injection.dart';
 export 'l10n/l10n.dart';
 export 'models/models.dart';
