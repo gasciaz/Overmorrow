2b49a427c15b2d6044b405be872bb184deeb496a
Dario Gasciarino <dario.gasciarino@outlook.it>
2025-08-19 18:02:34 +0200

    refactor(features): export forecast barrel file

--- lib/features/features.dart
+++ lib/features/features.dart
@@ -1,6 +1,7 @@
 export 'about/about.dart';
 export 'alerts/alerts.dart';
 export 'aqi/aqi.dart';
+export 'forecast/forecast.dart';
 export 'radar/radar.dart';
 export 'search/search.dart';
 export 'settings/settings.dart';
