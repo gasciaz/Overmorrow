8577f6ec770d3deb546d365432584d63f3a6e4e2
Dario Gasciarino <dario.gasciarino@outlook.it>
2025-08-19 17:47:22 +0200

    refactor(widget): use alerts barrel file

--- lib/core/widgets/new_displays.dart
+++ lib/core/widgets/new_displays.dart
@@ -21,9 +21,9 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:overmorrow/alerts_page.dart';
 import 'package:overmorrow/aqi_page.dart';
 import 'package:overmorrow/core/core.dart';
+import 'package:overmorrow/features/alerts/alerts.dart';
 
 class WavePainter extends CustomPainter {
   final double waveValue;
