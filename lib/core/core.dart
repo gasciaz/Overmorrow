43701357f7012d1e413e6f659b7c6ac7a355cca1
Dario Gasciarino <dario.gasciarino@outlook.it>
2025-08-19 18:04:19 +0200

    refactor(core): export utils barrel file

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -5,4 +5,5 @@ export 'di/dependency_injection.dart';
 export 'l10n/l10n.dart';
 export 'models/models.dart';
 export 'services/services.dart';
+export 'utils/utils.dart';
 export 'widgets/widgets.dart';
