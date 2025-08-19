25f22e971d96351f640afe8b81e828b60bcf893c
Dario Gasciarino <dario.gasciarino@outlook.it>
2025-08-19 17:08:30 +0200

    refactor(models): export models barrel file

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -2,4 +2,5 @@ export 'assets/assets.dart';
 export 'caching/caching.dart';
 export 'di/dependency_injection.dart';
 export 'l10n/l10n.dart';
+export 'models/models.dart';
 export 'services/services.dart';
