fc9e2893aa2d21910b1bcda57bd7d882bc8a4e7f
Dario Gasciarino <dario.gasciarino@outlook.it>
2025-08-19 17:28:05 +0200

    refactor(settings): import about barrel file

--- lib/settings_screens.dart
+++ lib/settings_screens.dart
@@ -19,8 +19,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-import 'package:overmorrow/about_page.dart';
 import 'package:overmorrow/core/core.dart';
+import 'package:overmorrow/features/about/about.dart';
 import 'package:overmorrow/main_ui.dart';
 import 'package:overmorrow/settings_page.dart';
 import 'package:overmorrow/weather_refact.dart';
